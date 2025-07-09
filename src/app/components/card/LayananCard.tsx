c26b694d7b1b49ee8aae9ed7a79fcbeeb6043a68
gitaufar <zhafiraufar123@gmail.com>
2025-07-09 22:02:44 +0700

    refactor: refactor database model

--- src/app/components/card/LayananCard.tsx
+++ src/app/components/card/LayananCard.tsx
@@ -11,8 +11,8 @@ type LayananCardProps = {
 
 const LayananCard = ({ title, description, imageUrl, onClick }: LayananCardProps) => {
   return (
-    <div className="gap-5 text-white text-center flex flex-col items-center py-8 px-7 border-4 border-white rounded-3xl">
-      <img src={imageUrl} alt={title} className="w-full rounded-2xl" />
+    <div className="gap-2 text-white text-center flex flex-col items-center py-8 px-7 border-4 border-white rounded-3xl">
+      <img src={imageUrl} alt={title} className="w-full rounded-2xl h-42" />
       <h2 className="font-bold text-4xl">{title}</h2>
       <p className="text-xl font-normal opacity-80">{description}</p>
       <ButtonGeneral text="Mulai Sekarang" onClick={() => onClick} />
