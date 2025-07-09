3972c8a0ff463b66cdba5c328354c451f3dc42fd
gitaufar <zhafiraufar123@gmail.com>
2025-07-09 22:43:50 +0700

    config flowbite

--- src/app/components/section/InformationSection.tsx
+++ src/app/components/section/InformationSection.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Button } from "flowbite-react";
 
 const InformationSection = () => {
   return (
-    <div>Udah selesai</div>
-  )
-}
+    <div className="flex flex-wrap gap-2">
+      <Button>Default</Button>
+      <Button color="alternative">Alternative</Button>
+      <Button color="dark">Dark</Button>
+      <Button color="light">Light</Button>
+      <Button color="green">Green</Button>
+      <Button color="red">Red</Button>
+      <Button color="yellow">Yellow</Button>
+      <Button color="purple">Purple</Button>
+    </div>
+  );
+};
 
-export default InformationSection
+export default InformationSection;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  content: [
+    // ... path Anda
+  ],
+  plugins: [
+    require('flowbite/plugin') // <- Ini memerlukan file config
+  ],
+}
