7f32afc495b6ce6ca7c705b3dce18d3bd8a26a1a
Rafifffff <rafifrantisi7@gmail.com>
2025-07-11 12:34:40 +0700

    dua section di home page

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
+import withFlowbiteReact from "flowbite-react/plugin/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    serverActions: true, 
+    
   },
 };
 
-export default nextConfig;
+export default withFlowbiteReact(nextConfig);
